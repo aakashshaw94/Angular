03c4b72d2d96dcb022ade883deaf92b0a801dd61
Aakash Shaw <aakashshaw@Aakashs-MacBook-Air.local>
2023-08-01 01:17:58 +0530

    Components and DataBinding

---
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.css']
+})
+export class RecipesListComponent implements OnInit{
+  recipes : Recipe[] = [
+    new Recipe('Recipe1', 'This is a test recipe', 'https://img.freepik.com/free-photo/chicken-wings-barbecue-sweetly-sour-sauce-picnic-summer-menu-tasty-food-top-view-flat-lay_2829-6471.jpg?w=2000'),
+    new Recipe('Recipe1', 'This is a test recipe', 'https://img.freepik.com/free-photo/chicken-wings-barbecue-sweetly-sour-sauce-picnic-summer-menu-tasty-food-top-view-flat-lay_2829-6471.jpg?w=2000'),
+    new Recipe('Recipe1', 'This is a test recipe', 'https://img.freepik.com/free-photo/chicken-wings-barbecue-sweetly-sour-sauce-picnic-summer-menu-tasty-food-top-view-flat-lay_2829-6471.jpg?w=2000'),
+    new Recipe('Recipe1', 'This is a test recipe', 'https://img.freepik.com/free-photo/chicken-wings-barbecue-sweetly-sour-sauce-picnic-summer-menu-tasty-food-top-view-flat-lay_2829-6471.jpg?w=2000'),
+    new Recipe('Recipe1', 'This is a test recipe', 'https://img.freepik.com/free-photo/chicken-wings-barbecue-sweetly-sour-sauce-picnic-summer-menu-tasty-food-top-view-flat-lay_2829-6471.jpg?w=2000')
+  ];
+
+  constructor() {
+  }
+  ngOnInit(): void {
+  }
+
+}
